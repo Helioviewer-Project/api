58ccc46e889539d2b01a296be92260760db5635e
Jeff Stys <jstys@sesda3.com>
2014-11-25 09:29:38 -0500

    Added 11 new test cases for SolarEvent APIs.

---
+++ tests/solarevents/getDefaultEventTypes_001.php
@@ -0,0 +1,28 @@
+<?php
+include_once __DIR__.'/../../tests/test_header.php';
+
+include_once __DIR__.'/../../src/Module/SolarEvents.php';
+include_once __DIR__.'/../../src/Validation/InputValidator.php';
+include_once __DIR__.'/../../src/Helper/ErrorHandler.php';
+
+
+$params = array(
+    'action'    => 'getDefaultEventTypes'
+);
+
+echo "\n\nInput to test case:\n";
+echo '$params => ';
+var_dump($params);
+
+echo "\nInitializing Object";
+$solarEvents = new Module_SolarEvents($params);
+
+echo "\n\nExecuting API call:\n";
+echo '$solarEvents->execute()'."\n";
+
+
+echo "\nActual Test Output:\n";
+$solarEvents->execute();
+
+include_once __DIR__.'/../../tests/test_footer.php';
+?>
---
+++ tests/solarevents/getEventFRMs_002.php
@@ -0,0 +1,30 @@
+<?php
+include_once __DIR__.'/../../tests/test_header.php';
+
+include_once __DIR__.'/../../src/Module/SolarEvents.php';
+include_once __DIR__.'/../../src/Validation/InputValidator.php';
+include_once __DIR__.'/../../src/Helper/ErrorHandler.php';
+
+
+$params = array(
+    'action'    => 'getEventFRMs',
+    'startTime' => '2014-02-28T00:00:00.000Z',
+    'ar_filter' => 'true'
+);
+
+echo "\n\nInput to test case:\n";
+echo '$params => ';
+var_dump($params);
+
+echo "\nInitializing Object";
+$solarEvents = new Module_SolarEvents($params);
+
+echo "\n\nExecuting API call:\n";
+echo '$solarEvents->execute()'."\n";
+
+
+echo "\nActual Test Output:\n";
+$solarEvents->execute();
+
+include_once __DIR__.'/../../tests/test_footer.php';
+?>
