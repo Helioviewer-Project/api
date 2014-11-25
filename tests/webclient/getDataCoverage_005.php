71387b6a52ce868d19b706cd16cabfcbcdf95b9b
Jeff Stys <jstys@sesda3.com>
2014-11-25 08:47:58 -0500

    Added 9 test cases for getDataCoverage. Added a test case for shortenURL. Removed two no-longer valid resolutions from getDataCoverage and getDataCoverageTimeline.

---
+++ tests/webclient/getDataCoverage_005.php
@@ -0,0 +1,30 @@
+<?php
+include_once __DIR__.'/../../tests/test_header.php';
+
+include_once __DIR__.'/../../src/Module/WebClient.php';
+include_once __DIR__.'/../../src/Validation/InputValidator.php';
+include_once __DIR__.'/../../src/Helper/ErrorHandler.php';
+
+
+$params = array(
+    'action'     => 'getDataCoverage',
+    'resolution' => '1W',
+    'endDate'    => '2014-03-31T23:59:59.999Z'
+);
+
+echo "\n\nInput to test case:\n";
+echo '$params => ';
+var_dump($params);
+
+echo "\nInitializing Object";
+$webClient = new Module_WebClient($params);
+
+echo "\n\nExecuting API call:\n";
+echo '$webClient->execute()'."\n";
+
+
+echo "\nActual Test Output:\n";
+$webClient->execute();
+
+include_once __DIR__.'/../../tests/test_footer.php';
+?>
