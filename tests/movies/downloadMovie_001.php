5cef874dfc329e56b9508a3c1b38d1cc996b1db0
Jeff Stys <jstys@sesda3.com>
2014-12-01 13:45:17 -0500

    Added new test cases for the following API actions:    + downloadMovie, getMovieStatus, playMovie, queueMovie, reQueueMovie

---
+++ tests/movies/downloadMovie_001.php
@@ -0,0 +1,41 @@
+<?php
+include_once __DIR__.'/../../tests/test_header.php';
+
+include_once __DIR__.'/../../src/Module/Movies.php';
+include_once __DIR__.'/../../src/Validation/InputValidator.php';
+include_once __DIR__.'/../../src/Helper/ErrorHandler.php';
+
+$params = array(
+    'action'  => 'downloadMovie',
+    'id'      => 'FJ0d5',
+    'format'  => 'mp4'
+);
+
+echo "\n\nInput to test case:\n";
+echo '$params => ';
+var_dump($params);
+
+echo "\nInitializing Object";
+$movies = new Module_Movies($params);
+
+echo "\n\nOutputting evidence that input was accepted:\n";
+echo '$movies => ';
+var_dump($movies);
+
+echo "\n\nDump binary data to stdout?";
+echo "\n'y' or 'n': ";
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+if ( strtolower(trim($line)) != 'y' ) {
+    echo "\n\nTest Script ABORTED: ".date('Y-m-d H:i:s T', time())."\n\n";
+    exit;
+}
+
+echo "\n\nExecuting API call:\n";
+echo '$movies->execute()'."\n";
+
+echo "\nActual Test Output:\n";
+$movies->execute();
+
+include_once __DIR__.'/../../tests/test_footer.php';
+?>
---
+++ tests/movies/getMovieStatus_002.php
@@ -0,0 +1,34 @@
+<?php
+include_once __DIR__.'/../../tests/test_header.php';
+
+include_once __DIR__.'/../../src/Module/Movies.php';
+include_once __DIR__.'/../../src/Validation/InputValidator.php';
+include_once __DIR__.'/../../src/Helper/ErrorHandler.php';
+
+$params = array(
+    'action'  => 'getMovieStatus',
+    'id'      => 'FJ0d5',
+    'format'  => 'webm',
+    'verbose' => 'true',
+    'callback' => 'callbackTest'
+);
+
+echo "\n\nInput to test case:\n";
+echo '$params => ';
+var_dump($params);
+
+echo "\nInitializing Object";
+$movies = new Module_Movies($params);
+
+echo "\n\nOutputting evidence that input was accepted:\n";
+echo '$movies => ';
+var_dump($movies);
+
+echo "\n\nExecuting API call:\n";
+echo '$movies->execute()'."\n";
+
+echo "\nActual Test Output:\n";
+$movies->execute();
+
+include_once __DIR__.'/../../tests/test_footer.php';
+?>
