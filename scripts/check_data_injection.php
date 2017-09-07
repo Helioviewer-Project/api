fec9f3ef7c8ffb7e73e8a36f4bdb73d190a5b948
Serge Zahniy <ddialer@gmail.com>
2017-09-07 13:02:10 -0400

    Add logging function to Data injection check script

--- scripts/check_data_injection.php
+++ scripts/check_data_injection.php
@@ -1,22 +1,45 @@
 <?php
+	function grab_dump($var){
+	    ob_start();
+	    var_dump($var);
+	    return ob_get_clean();
+	}
+
+	$cPath = ini_get('include_path');
+	ini_set('include_path', ini_get('include_path').':/usr/local/bin:/usr/bin:/bin');
+	
 	require_once __DIR__."/../src/Config.php";
 	$config = new Config(__DIR__."/../settings/Config.ini");
 	
 	$commands = unserialize(TERMINAL_COMMANDS);
 	$sendEmail = false;
-	$commandsExecuted = '';
+	$log = true;
 	
 	$output = shell_exec('ps -ef | grep python');
 	
+	$logBody = "////////////////////\n".date("Y-m-d H:i:s")."\n";
+	$logBody .= $output."";
+	//$strOut = array();
     foreach($commands as $cmd => $name){
-        if (strpos($output, $cmd) !== false) {
+        $is = strpos($output, $cmd);
+        //$strOut[] = grab_dump($is);
+        if ($is !== false) {
 	        //Command is running. Continue...
+	        $logBody .= 'SKIPPING: '.$name."\n";
         }else{
-	        $sendEmail = true;
-	        $output = shell_exec('nohup '.$cmd.' > /dev/null 2>/dev/null &');
-			$commandsExecuted .= 'nohup '.$cmd.'&<br/>';
+	        $log = true;
+	        $outputExec = shell_exec('PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/gems/1.8/bin nohup '.$cmd.' > /dev/null 2>/dev/null &');
+			$logBody .= 'RUNNING: '.$name." (".$cmd.")\n";
         }
     }
+	
+	
+	//Write log
+	if($log){
+		//$logBody .= ''.print_r($strOut, true)."\n";
+		file_put_contents('/var/www/api.helioviewer.org/scripts/check_data_injection.log', $logBody."\n\n", FILE_APPEND);
+	}
+	
 	
 	//Send email
 	/*
@@ -29,7 +52,7 @@
 		
 		$headers = "From: " .($from) . "\r\n";
 		$headers .= "Reply-To: ".($from) . "\r\n";
-		$headers .= "Return-Path: ".($from) . "\r\n";;
+		$headers .= "Return-Path: ".($from) . "\r\n";
 		$headers .= "MIME-Version: 1.0\r\n";
 		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 		$headers .= "X-Priority: 3\r\n";
