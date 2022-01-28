c3dc0cd37b2b8e3ed3f94df0f31966793d5bdc20
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-01-28 10:41:59 -0500

    Revert "Add lag_minutes to framework to account for datasource lagging"
    This reverts commit 8c9781d10861e8a55500e3e2fb78e992eb6f3ecf.

--- install/helioviewer/hvpull/servers/__init__.py
+++ install/helioviewer/hvpull/servers/__init__.py
@@ -8,15 +8,15 @@ class DataServer:
         self.uri = uri
         self.name = name
         self.pause = datetime.timedelta(minutes=pause)
-
+        
         # Example: 2011_11_17__08_13_08_13__SDO_AIA_AIA_304.jp2
         self.filename_regex = (
             "^(?P<year>\d{4})_(?P<month>\d{2})_(?P<day>\d{2})__" +
-            "(?P<hour>\d{2})_(?P<min>\d{2})_(?P<sec>\d{2})_" +
+            "(?P<hour>\d{2})_(?P<min>\d{2})_(?P<sec>\d{2})_" + 
             "(?P<microsec>\d{2,3})__" +
             "(?P<obs>[a-zA-Z0-9]{3})_(?P<inst>[a-zA-Z0-9]{3})_" +
             "(?P<det>[a-zA-Z0-9]{3})_(?P<meas>[a-zA-Z0-9]{2,11})\.jp2$")
-
+        
     def compute_directories(self, start_date, end_date):
         """Creates a list of possible directories containing new files"""
         return []
@@ -24,7 +24,7 @@ class DataServer:
     def get_starttime(self):
         """Default start time to use when retrieving data"""
         return datetime.datetime.utcnow() - datetime.timedelta(hours=6)
-
+   
     def get_dates(self, starttime, endtime):
         """Get a complete list of dates between the start and the end time"""
         fmt = "%Y/%m/%d"
@@ -34,13 +34,13 @@ class DataServer:
         while date < endtime.date():
             date = date + datetime.timedelta(days=1)
             dates.append(date.strftime(fmt))
-
+        
         # Ensure the dates are most recent first
         dates.sort()
         dates.reverse()
-
+        
         return dates
-
+    
     def get_file_regex(self):
         """Returns a regex which described the expected format of filenames on
         the server"""
@@ -49,7 +49,7 @@ class DataServer:
     def get_measurements(self, nicknames, dates):
         """Get a list of all the URIs down to the measurement"""
         return None
-
+    
     def get_uri(self):
         """Return the server URI"""
         return self.uri
@@ -58,11 +58,10 @@ class DataServerPauseDelayDefinesDefaultStartTime:
     """Class for interacting with data servers. In this class the
     pause defines the default start time. If real time is UTC, then
     the default start time is UTC - pause minutes."""
-    def __init__(self, uri, name, pause=60, lag_minutes=0):
+    def __init__(self, uri, name, pause=60):
         self.uri = uri
         self.name = name
         self.pause = datetime.timedelta(minutes=pause)
-        self.lag_minutes = datetime.timedelta(minutes=lag_minutes)
 
         # Example: 2011_11_17__08_13_08_13__SDO_AIA_AIA_304.jp2
         self.filename_regex = (
@@ -78,7 +77,7 @@ class DataServerPauseDelayDefinesDefaultStartTime:
 
     def get_starttime(self):
         """Default start time to use when retrieving data"""
-        return datetime.datetime.utcnow() - self.pause - self.lag_minutes
+        return datetime.datetime.utcnow() - self.pause
 
     def get_dates(self, starttime, endtime):
         """Get a complete list of dates between the start and the end time"""
--- install/helioviewer/hvpull/servers/lmsal2.py
+++ install/helioviewer/hvpull/servers/lmsal2.py
@@ -10,22 +10,22 @@ class LMSALDataServer2(DataServerPauseDelayDefinesDefaultStartTime):
     """LMSAL Datasource definition"""
     def __init__(self):
         """Defines the root directory of where the data is kept at LMSAL."""
-        DataServerPauseDelayDefinesDefaultStartTime.__init__(self, "http://sdowww.lmsal.com/sdomedia/hv_jp2kwrite/v0.8/jp2/", "LMSAL2", lag_minutes=45)#http://www.lmsal.com/sdomedia/hv_jp2kwrite/v0.8/jp2/
-
+        DataServerPauseDelayDefinesDefaultStartTime.__init__(self, "http://sdowww.lmsal.com/sdomedia/hv_jp2kwrite/v0.8/jp2/", "LMSAL2")#http://www.lmsal.com/sdomedia/hv_jp2kwrite/v0.8/jp2/
+        
     def compute_directories(self, start_date, end_date):
         """Computes a list of remote directories expected to contain files"""
         dirs = []
-
+        
         aia_wavelengths = [4500, 304, 171, 1600, 193, 211, 335, 131, 94, 1700]
         hmi_measurements = ["continuum", "magnetogram"]
-
+        
         for date in self.get_dates(start_date, end_date):
             # AIA
             for meas in aia_wavelengths:
                 dirs.append(os.path.join(self.uri, "AIA", date, str(meas)))
-
+            
             # HMI
             for meas in hmi_measurements:
                 dirs.append(os.path.join(self.uri, "HMI", date, meas))
-
+                
         return dirs
