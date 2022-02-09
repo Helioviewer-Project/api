b725b9d84411da90a118f097cb3adf3a651f5da9
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-02-09 13:22:42 -0500

    Add query to patch existing xrt rows.

---
+++ install/database/2022_02_09_xrt_group_patch.sql
@@ -0,0 +1,37 @@
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10008 WHERE sourceId = 38; -- Al_med/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10008 WHERE sourceId = 39; -- Al_med/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10008 WHERE sourceId = 40; -- Al_med/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10008 WHERE sourceId = 41; -- Al_med/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10008 WHERE sourceId = 42; -- Al_med/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10008 WHERE sourceId = 43; -- Al_med /Ti_poly
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10009 WHERE sourceId = 44; -- Al_poly/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10009 WHERE sourceId = 45; -- Al_poly/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10009 WHERE sourceId = 46; -- Al_poly/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10009 WHERE sourceId = 47; -- Al_poly/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10009 WHERE sourceId = 48; -- Al_poly/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10009 WHERE sourceId = 49; -- Al_poly/Ti_poly
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10010 WHERE sourceId = 50; -- Be_med/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10010 WHERE sourceId = 51; -- Be_med/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10010 WHERE sourceId = 52; -- Be_med/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10010 WHERE sourceId = 53; -- Be_med/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10010 WHERE sourceId = 54; -- Be_med/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10010 WHERE sourceId = 55; -- Be_med/Ti_poly
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10011 WHERE sourceId = 56; -- Be_thin/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10011 WHERE sourceId = 57; -- Be_thin/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10011 WHERE sourceId = 58; -- Be_thin/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10011 WHERE sourceId = 59; -- Be_thin/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10011 WHERE sourceId = 60; -- Be_thin/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10011 WHERE sourceId = 61; -- Be_thin/Ti_poly
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10012 WHERE sourceId = 62; -- C_poly/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10012 WHERE sourceId = 63; -- C_poly/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10012 WHERE sourceId = 64; -- C_poly/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10012 WHERE sourceId = 65; -- C_poly/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10012 WHERE sourceId = 66; -- C_poly/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10012 WHERE sourceId = 67; -- C_poly/Ti_poly
+UPDATE data SET groupOne = 10001, groupTwo = 0    , groupThree = 0     WHERE sourceId = 68; -- Mispositioned/Mispositioned
+UPDATE data SET groupOne = 10001, groupTwo = 10002, groupThree = 10013 WHERE sourceId = 69; -- Open/Al_mesh
+UPDATE data SET groupOne = 10001, groupTwo = 10003, groupThree = 10013 WHERE sourceId = 70; -- Open/Al_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10004, groupThree = 10013 WHERE sourceId = 71; -- Open/Be_thick
+UPDATE data SET groupOne = 10001, groupTwo = 10005, groupThree = 10013 WHERE sourceId = 72; -- Open/Gband
+UPDATE data SET groupOne = 10001, groupTwo = 10006, groupThree = 10013 WHERE sourceId = 73; -- Open/Open
+UPDATE data SET groupOne = 10001, groupTwo = 10007, groupThree = 10013 WHERE sourceId = 74; -- Open/Ti_poly
