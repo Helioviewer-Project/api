52c775eab255c8f3d88a2729ec22b170d23767d1
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2022-03-30 12:05:58 -0400

    Set some initial thresholds for AIA, LASCO, and STEREO

--- scripts/availability_feed/check_status.py
+++ scripts/availability_feed/check_status.py
@@ -12,7 +12,9 @@ import requests
 # If AIA seconds behind is greater than this value, then it is a problem.
 # AIA should not be more than 2 hours behind since helioviewer.org pulls
 # every hour.
-AIA_LAG_THRESHOLD_S = 7200
+AIA_LAG_THRESHOLD_S    =   7200    # 2 hours
+LASCO_LAG_THRESHOLD_S  =  14400 # 4 hours
+SECCHI_LAG_THRESHOLD_S = 259200 # 3 days
 
 def query_status_api():
     host = get_host()
@@ -20,13 +22,23 @@ def query_status_api():
     resp = requests.get(api_endpoint)
     return resp.json()
 
+def check_for_lag(source: str, threshold_seconds: float, seconds_behind: int):
+    if (seconds_behind > threshold_seconds):
+        threshold_hours = threshold_seconds / 60 / 60
+        hours_behind = seconds_behind / 60 / 60
+        print("%s image are behind by %0.2f hours. Alert threshold is %0.2f hours." % (source, hours_behind, threshold_hours))
+
 def check_status(status):
     aia_seconds_behind = status['AIA']['secondsBehind']
-    if aia_seconds_behind > AIA_LAG_THRESHOLD_S:
-        print("AIA/HMI images are not current.")
+    check_for_lag("AIA/HMI", AIA_LAG_THRESHOLD_S, aia_seconds_behind)
 
-    # TODO: Set a threshold for lasco
-    # soho_seconds_behind = status['LASCO']['secondsBehind']
+    # this covers SOHO
+    lasco_seconds_behind = status['LASCO']['secondsBehind']
+    check_for_lag("LASCO", LASCO_LAG_THRESHOLD_S, lasco_seconds_behind)
+
+    # this coveres STEREO
+    secchi_seconds_behind = status['SECCHI']['secondsBehind']
+    check_for_lag("STEREO Source", SECCHI_LAG_THRESHOLD_S, secchi_seconds_behind)
     pass
 
 if __name__ == "__main__":
