478d62ca00ace85324463f971a8f25116176772a
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2022-04-26 14:45:55 -0400

    Set shared to 0 on private youtube videos (#134)

---
+++ tests/unit_tests/movies/MovieDatabaseTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+ * @author Daniel Garcia-Briseno <daniel.garciabriseno@nasa.gov>
+ */
+
+use PHPUnit\Framework\TestCase;
+
+// File under test
+include_once HV_ROOT_DIR.'/../src/Net/Proxy.php';
+include_once HV_ROOT_DIR.'/../src/Database/MovieDatabase.php';
+
+final class MovieDatabaseTest extends TestCase
+{
+    public function testGetForbiddenYoutubeVideo()
+    {
+        $db = new Database_MovieDatabase();
+        $response = $db->getYoutubeVideo("0X6f5V8V29c");
+        $this->assertEquals('Forbidden', $response);
+    }
+}
