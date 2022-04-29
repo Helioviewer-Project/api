ac3c87e0b33ca95274a63d5b89be1ec5df9d252e
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2022-04-29 12:47:04 -0400

    Update health check to check on movie queue workers

--- scripts/availability_feed/health_check.sh
+++ scripts/availability_feed/health_check.sh
@@ -2,13 +2,13 @@
 # This script is use to perform various status checks on helioviewer.org
 # and update the XML feed if a problem is detected.
 # this script is expected to run on a cron job.
-
+set -x
 # Exit the script if the lockfile exists. The lockfile is created once
 # an error is detected
 lockfile=feed.lock
 if [ -f $lockfile ]
 then
-    exit
+    exit 0
 fi
 
 # Set HV_FEED_HOST if it hasn't been set by the environment
@@ -32,5 +32,13 @@ result=`python3 check_status.py`
 if [ ! -z "$result" ]
 then
     python3 gen_feed.py ../../docroot/status.xml -t "Helioviewer images are behind schedule." -d "$result"
+    touch $lockfile
+fi
+
+# Check that movie queues are active
+queue_count=`resque list | wc -l`
+if [ $queue_count -le 1 ]
+then
+    python3 gen_feed.py ../../docroot/status.xml -t "Movie Generation is Down." -d "Movie queue workers are not running"
     touch $lockfile
 fi
