2234186cdc86d003331b3e54e779ca38a2e62385
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-02-15 14:30:35 -0500

    Add dependencies for hv_stats

--- scripts/hv_stats/generate_bokeh_plots.sh
+++ scripts/hv_stats/generate_bokeh_plots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "##### Starting Bokeh Plot Generation Script #####"
 date
-/usr/bin/python3 hv_stats.py
+python hv_stats.py
 echo "Copying Files to Web Directory api.helioviewer.org/docroot/statistics/bokeh/"
 cp -r Jhv_movies histograms hv_movies hv_screenshots hv_student coverages embed_service service_usage /var/www/api.helioviewer.org/docroot/statistics/bokeh/
 date
