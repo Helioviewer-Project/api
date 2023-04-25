4a0948115c81c76026e7371a5994000a4d654707
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-04-25 10:19:02 -0400

    Migrate flare predictions into the Helioviewer Event Interface (#300)
    RELNOTE: importing flare predictions is no longer required.

--- src/Helper/EventInterface.php
+++ src/Helper/EventInterface.php
@@ -6,16 +6,16 @@ include_once HV_ROOT_DIR.'/../scripts/rot_hpc.php';
 use HelioviewerEventInterface\Events;
 
 class Helper_EventInterface {
-    public static function GetEvents(DateTimeInterface $start, DateTimeInterface $end, string $observationTime, ?array $sources = null): array {
+    public static function GetEvents(DateTimeInterface $start, DateInterval $length, string $observationTime, ?array $sources = null): array {
         $applyRotation = function ($hv_event) use ($observationTime) {
             // Apply solar rotation from the event time to the current observation time
             list($hv_event->hv_hpc_x, $hv_event->hv_hpc_y) = rot_hpc($hv_event->hpc_x, $hv_event->hpc_y, $hv_event->start, $observationTime);
             return $hv_event;
         };
         if (is_null($sources)) {
-            return Events::GetAll($start, $end, $applyRotation);
+            return Events::GetAll($start, $length, $applyRotation);
         } else {
-            return Events::GetFromSource($sources, $start, $end, $applyRotation);
+            return Events::GetFromSource($sources, $start, $length, $applyRotation);
         }
     }
 }
