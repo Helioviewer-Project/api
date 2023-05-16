a35297e243e808b560a1a80434ae737f6076bfe6
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-05-16 11:46:03 -0400

    Add 2796 measurement

--- install/database/2023_05_16_iris_source.sql
+++ install/database/2023_05_16_iris_source.sql
@@ -1,9 +1,13 @@
 INSERT INTO datasources (id, name, description, units, layeringOrder, enabled, sourceIdGroup, displayOrder)
 VALUES
-(88, 'IRIS SJI 1330', 'IRIS SJI 1330',  NULL, 1, 0, '', 0);
+(88, 'IRIS SJI 1330', 'IRIS SJI 1330',  NULL, 1, 0, '', 0),
+(89, 'IRIS SJI 2796', 'IRIS SJI 2796',  NULL, 1, 0, '', 0);
 
 INSERT INTO datasource_property (sourceId, label, name, fitsName, description, uiOrder)
 VALUES
 (88, 'Observatory', 'IRIS', 'IRIS', 'IRIS', 1),
 (88, 'Instrument', 'SJI', 'SJI', 'Slit Jaw Imager', 2),
-(88, 'Measurement', '1330', '1330', '1330 Ångström', 3);
+(88, 'Measurement', '1330', '1330', '1330 Ångström', 3),
+(89, 'Observatory', 'IRIS', 'IRIS', 'IRIS', 1),
+(89, 'Instrument', 'SJI', 'SJI', 'Slit Jaw Imager', 2),
+(89, 'Measurement', '2796', '2796', '2796 Ångström', 3);
