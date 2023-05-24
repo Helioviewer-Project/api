e3d27b340929c5ef606edebe8148df1734cd03c5
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-05-24 09:05:30 -0400

    Update hv_iris.py to inherit IRISDataServer

--- install/helioviewer/hvpull/servers/hv_iris.py
+++ install/helioviewer/hvpull/servers/hv_iris.py
@@ -3,12 +3,13 @@ import os
 import re
 from datetime import datetime
 from helioviewer.hvpull.servers import DataServer
+from helioviewer.hvpull.servers.iris import IRISDataServer
 
-class HvIRISDataServer(DataServer):
+class HvIRISDataServer(IRISDataServer):
     """IRIS Datasource definition"""
     def __init__(self):
         """Defines the root directory of where the data is kept on Helioviewer."""
-        DataServer.__init__(self, "https://helioviewer.org/jp2/", "IRIS")
+        DataServer.__init__(self, "https://api.beta.helioviewer.org/jp2/", "IRIS")
 
     def compute_directories(self, start_date, end_date):
         """Computes a list of remote directories expected to contain files"""
@@ -17,5 +18,7 @@ class HvIRISDataServer(DataServer):
         wavelengths = [1330, 1400, 2796, 2832]
 
         for date in self.get_dates(start_date, end_date):
-            for meas in aia_wavelengths:
+            for meas in wavelengths:
                 dirs.append(os.path.join(self.uri, "SJI", date, str(meas)))
+
+        return dirs
