228eba6c43d0395704d1350b8655566567b84fff
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-08 16:39:36 -0400

    Fallback to CRVALn = 0 when CRVALn is not in the header

--- tests/unit_tests/database/ImgIndexTest.php
+++ tests/unit_tests/database/ImgIndexTest.php
@@ -12,13 +12,21 @@ include_once HV_ROOT_DIR.'/../src/Database/DbConnection.php';
 final class ImgIndexTest extends TestCase
 {
     public function test_getDataRange(): void
-    {   
+    {
         $start = '2022-01-01 00:00:00';
         $end = '2022-07-01 00:00:00';
         $sourceId = 8; // AIA 94
         $imgIndex = new Database_ImgIndex();
         $result = $imgIndex->getDataRange($start, $end, $sourceId);
         $this->assertLessThan(HV_MAX_ROW_LIMIT, count($result));
-    }   
+    }
+
+    public function test_processBadXml(): void {
+        $file = __DIR__ . "/test_data/2005_01_11__23_59_32_227__SOHO_MDI_MDI_continuum.jp2";
+        $imgIndex = new Database_ImgIndex();
+        $meta = $imgIndex->extractJP2MetaInfo($file);
+        $this->assertEquals(1024, $meta['width']);
+        $this->assertEquals(1024, $meta['height']);
+    }
 }
 
