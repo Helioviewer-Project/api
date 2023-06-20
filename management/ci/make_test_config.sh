f72a0bb80672b5e86b9835e6281f2305eaac64ad
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-20 13:40:27 -0400

    Updates to ci scripts
    - make CI wait until container is ready
    - Update make_test_config to conform to script standards

--- management/ci/make_test_config.sh
+++ management/ci/make_test_config.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+cmd=$0
+usage() {
+    echo "Usage: "
+    echo "  $cmd <Config.ini path> <Private.php path>"
+    echo
+    echo "Creates a Config.ini and Private.php to use for testing"
+    echo "The provided paths are where the test configuration files are written"
+}
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] || [[ $# -ne 2 ]]; then
+    usage
+    exit 0
+fi
+
+CONFIG_INI=$1
+PRIVATE_PHP=$2
+
+# Make Config.ini
 sed "s:/var/www-api/docroot/cache:/home/helioviewer/helioviewer.org/cache:" settings/Config.Example.ini | \
 sed "s:/var/www-api/docroot:/home/helioviewer/api.helioviewer.org/docroot:" | \
-sed "s-http://localhost-http://127.0.0.1-"
+sed "s-http://localhost-http://127.0.0.1-" > $CONFIG_INI
+
+# Make Private.php
+sed "s/localhost/127.0.0.1/" settings/Private.Example.php > $PRIVATE_PHP
