f5a74818f53d421475e6780302f63a2fbf8eb6c7
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-20 15:01:58 -0400

    update container_setup

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s $PWD /home/helioviewer/api.helioviewer.org
-nohup bash /home/helioviewer/setup_files/scripts/startup.sh > startup_output &
-timeout 120s bash -c 'until [[ "$(tail -n 1 startup_output)" == "Container up and running" ]]; do
+nohup bash /home/helioviewer/setup_files/scripts/startup.sh > /tmp/startup_output &
+timeout 90s bash -c 'until [[ "$(tail -n 1 /tmp/startup_output)" == "Container up and running" ]]; do
+    echo waiting for container to be ready
     sleep 1
-    tail -n 1 startup_output
 done'
