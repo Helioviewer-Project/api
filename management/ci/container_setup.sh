268f4825543f5ea23c56427a61f09e5b5ddc651d
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-20 15:27:28 -0400

    debug output

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -2,5 +2,7 @@
 echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s $PWD /home/helioviewer/api.helioviewer.org
 cd /home/helioviewer/setup_files/scripts/
-sed "s:read::" startup.sh > startup2.sh
+echo set -x\n > startup2.sh
+sed "s:read::" startup.sh >> startup2.sh
 bash startup2.sh
+echo completed startup script
