420a2cc8c0789c5de2d22c2b18d584ba56d67290
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-20 15:10:37 -0400

    timeout on script?

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s $PWD /home/helioviewer/api.helioviewer.org
-nohup bash /home/helioviewer/setup_files/scripts/startup.sh > /tmp/startup_output &
-sleep 90
+timeout 90s bash /home/helioviewer/setup_files/scripts/startup.sh
