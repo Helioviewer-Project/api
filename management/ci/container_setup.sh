ecc366ab5ebc196478addaa9c3806fb53c72d187
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-20 15:05:17 -0400

    just sleep

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -2,7 +2,4 @@
 echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s $PWD /home/helioviewer/api.helioviewer.org
 nohup bash /home/helioviewer/setup_files/scripts/startup.sh > /tmp/startup_output &
-timeout 90s bash -c 'until [[ "$(tail -n 1 /tmp/startup_output)" == "Container up and running" ]]; do
-    echo waiting for container to be ready
-    sleep 1
-done'
+sleep 90
