b666aced76f5adc4c7fe6cc45b19620f0bbbe0bb
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-21 12:53:03 -0400

    just sleep

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -3,6 +3,4 @@ echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ln -s $PWD /home/helioviewer/api.helioviewer.org
 cd /home/helioviewer/setup_files/scripts/
 ./startup.sh > startup_log &
-until [[ "$(tail -n 1 startup_log)" == "Container up and running" ]]; do
-    sleep 1
-done
+sleep 60
