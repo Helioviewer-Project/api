9d519e0b321df3d76acb85bfe2ef24e89b076fa2
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-21 11:22:57 -0400

    testing nohup

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-ln -s $PWD /home/helioviewer/api.helioviewer.org
-cd /home/helioviewer/setup_files/scripts/
-echo set -x\; > startup2.sh
-sed "s:read::" startup.sh >> startup2.sh
-bash startup2.sh
-echo completed startup script
-exit 1
+nohup sleep 60 &
+# echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# ln -s $PWD /home/helioviewer/api.helioviewer.org
+# cd /home/helioviewer/setup_files/scripts/
+# echo set -x\; > startup2.sh
+# sed "s:read::" startup.sh >> startup2.sh
+# bash startup2.sh
+# echo completed startup script
+# exit 1
