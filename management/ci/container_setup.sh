6a3ab5c89872421951a145b59db10d3984238e49
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-21 14:02:15 -0400

    30s startup

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -s $PWD /home/helioviewer/api.helioviewer.org
 chown -R helioviewer:helioviewer .
-su helioviewer -c "nohup /home/helioviewer/setup_files/scripts/startup.sh &"
-sleep 60
+su helioviewer -c "bash -c \"/home/helioviewer/setup_files/scripts/startup.sh > startup.log &\""
+sleep 30
