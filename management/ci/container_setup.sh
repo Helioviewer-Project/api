8823fcb730022c21da27ac15696c1311320d3860
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-21 11:40:51 -0400

    subshell composer

--- management/ci/container_setup.sh
+++ management/ci/container_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-sleep 60 &
-# echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-# ln -s $PWD /home/helioviewer/api.helioviewer.org
-# cd /home/helioviewer/setup_files/scripts/
-# echo set -x\; > startup2.sh
-# sed "s:read::" startup.sh >> startup2.sh
-# bash startup2.sh
-# echo completed startup script
-# exit 1
+echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+ln -s $PWD /home/helioviewer/api.helioviewer.org
+cd /home/helioviewer/setup_files/scripts/
+./startup.sh > startup_log &
+until [[ "$(tail -n 1 startup_log)" == "Container up and running" ]]; do
+    sleep 1
+done
