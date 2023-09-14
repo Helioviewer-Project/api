12930802563108244b47bfa8cd040a4b69319574
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-09-14 14:08:59 -0400

    Exclude device data before we started tracking devices

--- src/Database/Statistics.php
+++ src/Database/Statistics.php
@@ -784,7 +784,6 @@ class Database_Statistics {
         $new_counts['summary'] = $new_summary;
         $new_counts['rate_limit_exceeded'] = $rateLimitExceeded;
         // 'x' represents requests before we started logging devices
-        $device_counts['unknown'] = $device_counts['x'] ?? 0;
         unset($device_counts['x']);
 
         // 'UNK' represents clients that the device detector didn't recognize.
