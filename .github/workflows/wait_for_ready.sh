6441d74a0ab8eac77ad607c2e8b09cbed400596d
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-12-15 12:35:20 -0500

    Also wait for api to be ready

--- .github/workflows/wait_for_ready.sh
+++ .github/workflows/wait_for_ready.sh
@@ -2,7 +2,10 @@
 for i in {1..12}; do
     docker exec -t helioviewer-cli-1 ls /tmp/jp2/LASCO-C2/2023/12/01/white-light/
     if [ $? -eq 0 ]; then
-    exit 0
+        docker exec -t helioviewer-api-1 ls /tmp/container_ready
+        if [ $? -eq 0 ]; then
+            exit 0
+        fi
     fi
     sleep 10
 done
