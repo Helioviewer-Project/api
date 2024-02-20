8ab7f6dbe863d76f78e0abf37710b0c764184dfe
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-02-20 10:33:03 -0500

    update color table generator

--- scripts/gen_color_tables/gen_rgb.py
+++ scripts/gen_color_tables/gen_rgb.py
@@ -22,9 +22,8 @@ if __name__ == "__main__":
                     prog='Gen RGB',
                     description='Generates text-based RGB color tables for further processing')
     parser.add_argument('name', help="Name of the color table to be created")
-    parser.add_argument('fn', help="Name of the sunpy color table function")
-    parser.add_argument('-a', '--args', help="Args to pass to the color table function", nargs="+")
+    parser.add_argument('cm', help="Name of the color map variable in sunpy.visualization.colormaps.cm")
     args = parser.parse_args()
-    import sunpy.visualization.colormaps.color_tables as ct
-    cmap = ct.__dict__[args.fn](*args.args)
+    import sunpy.visualization.colormaps.cm as cm
+    cmap = cm.__dict__[args.cm]
     gen_rgb(cmap, args.name)
--- scripts/gen_color_tables/gen_rgb.sh
+++ scripts/gen_color_tables/gen_rgb.sh
@@ -1,19 +1,17 @@
 # All-in-one script to create color tables for new Helioviewer images
 usage() {
-    echo "Usage: ./gen_rgb.sh name sunpy_fn sunpy_fn_args"
+    echo "Usage: ./gen_rgb.sh name sunpy_var"
     echo ""
     echo "Creates color tables for use in Helioviewer"
-    echo "See https://github.com/sunpy/sunpy/blob/main/sunpy/visualization/colormaps/color_tables.py for available functions"
+    echo "See https://github.com/sunpy/sunpy/blob/main/sunpy/visualization/colormaps/cm.py for available options"
 }
-if [ $# -lt 3 ]; then
+if [ $# -lt 2 ]; then
     usage
     exit
 fi
 
 name=$1
-sunpy_fn=$2
-shift; shift
-sunpy_args=$@
+sunpy_var=$2
 
-python gen_rgb.py "${name}_${sunpy_args}" $sunpy_fn -a $sunpy_args
-php gen_color_table.php "${name}_${sunpy_args}"
+python gen_rgb.py "${name}_${sunpy_args}" $sunpy_var
+php gen_color_table.php "${name}"
