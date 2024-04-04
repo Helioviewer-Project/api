d84d9c149595a9b2a35d6906e2f5930705c1d60a
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-04-04 13:01:28 -0400

    Update rhessi dataserver

--- install/helioviewer/hvpull/servers/rhessi.py
+++ install/helioviewer/hvpull/servers/rhessi.py
@@ -10,12 +10,17 @@ class RHESSIDataServer(DataServer):
         then copied over to a directory on the main Helioviewer server, from
         which it can be picked up by the ingestion services.  Note that
         a full path is required to specify the location of the data."""
-        DataServer.__init__(self, "~/api/jp2_ingest", "RHESSI")
-        self.pause = datetime.timedelta(seconds=1)
+        DataServer.__init__(self, "https://hesperia.gsfc.nasa.gov/~kim/rhessi_helioviewer/hv_input_files/rhessiwrite/v0.8/jp2/RHESSI/", "RHESSI")
+        self.pause = datetime.timedelta(seconds=60)
 
     def compute_directories(self, start_date, end_date):
         """Computes a list of remote directories expected to contain files"""
-        return [self.uri]
+        dirs = []
+        bands = ["3-6keV", "6-12keV", "12-25keV", "25-50keV", "50-100keV", "100-300keV"]
+        for date in self.get_dates(start_date, end_date):
+            for energy_band in bands:
+                dirs.append(os.path.join(self.uri, date, energy_band))
+        return dirs
 
     def get_starttime(self):
         """Default start time to use when retrieving data"""
