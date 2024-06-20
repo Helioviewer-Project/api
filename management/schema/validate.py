b28e04963f76ceb958c998b4cb158d2208920b79
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-06-20 08:04:16 -0400

    Update validate.py to use local files

--- management/schema/validate.py
+++ management/schema/validate.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser, Namespace
+from pathlib import Path
 import json
-from jsonschema import validate
+from jsonschema import validators, validate
+from referencing import Registry, Resource
+from referencing.exceptions import NoSuchResource
 
 def parse_args() -> Namespace:
     parser = ArgumentParser(description="Validates a json file against a json schema")
@@ -12,8 +15,24 @@ def load_json(json_file: str) -> dict:
     with open(json_file, "r") as fp:
         return json.load(fp)
 
+def retrieve_from_filesystem(uri: str):
+    api_prefix = "https://api.helioviewer.org/schema/"
+    schema_path = Path("../../docroot/schema/")
+    if not uri.startswith(api_prefix):
+        raise NoSuchResource(ref=uri)
+    path = schema_path / Path(uri.removeprefix(api_prefix))
+    contents = json.loads(path.read_text())
+    return Resource.from_contents(contents)
+
+def validate_schema(json: dict, schema: dict):
+    validator_class = validators.validator_for(schema)
+    # Registry which maps api.helioviewer.org paths to local ones
+    local_registry = Registry(retrieve=retrieve_from_filesystem)
+    validator = validator_class(schema, registry=local_registry)
+    validator.validate(json)
+
 if __name__ == "__main__":
     args = parse_args()
     schema = load_json(args.schema)
     json_file = load_json(args.json)
-    validate(json_file, schema)
+    validate_schema(json_file, schema)
