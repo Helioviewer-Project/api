e3bffbff1d1f3eb4309d982237ea3905f13d066f
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-06-20 08:15:04 -0400

    Use data provider for schema tests

--- tests/unit_tests/validation/SchemaValidatorTest.php
+++ tests/unit_tests/validation/SchemaValidatorTest.php
@@ -20,15 +20,15 @@ final class SchemaValidatorTest extends TestCase
     /**
      * Reads the list of test files from the test data folder
      * and returns test data in the format:
-     * array(
-     *   "test_file" => SHOULD_PASS/SHOULD_FAIL,
-     *   ...
-     * )
+     * [
+     *  ["test_file", SHOULD_PASS/SHOULD_FAIL],
+     *  ...
+     * ]
      *
      * All files in the valid folder should pass validation.
      * All files in the invalid folder should fail validation.
      */
-    private static function GetTestData(): array {
+    public static function GetTestData(): array {
         $test_data = [];
 
         $valid_files =  scandir(__DIR__ . "/test_data/valid");
@@ -36,7 +36,7 @@ final class SchemaValidatorTest extends TestCase
             if ($file == "." || $file == "..") {
                 continue;
             }
-            $test_data["valid/".$file] = SHOULD_PASS;
+            array_push($test_data, ["valid/$file", SHOULD_PASS]);
         }
 
         $invalid_files = scandir(__DIR__ . "/test_data/invalid");
@@ -44,45 +44,41 @@ final class SchemaValidatorTest extends TestCase
             if ($file == "." || $file == "..") {
                 continue;
             }
-            $test_data["invalid/".$file] = SHOULD_FAIL;
+            array_push($test_data, ["invalid/$file", SHOULD_FAIL]);
         }
         return $test_data;
     }
 
-    public function test_ValidateJsonSchema(): void
+    /**
+     * @dataProvider GetTestData
+     */
+    public function test_ValidateJsonSchema(string $json_file, bool $expected_result): void
     {
-        // Get all test files
-        $test_data = SchemaValidatorTest::GetTestData();
-
-        // Run the validator on every file and verify the expected pass/fail result.
-        foreach ($test_data as $json_file => $expected_result) {
-            $schema = "https://api.helioviewer.org/schema/client_state.schema.json";
-            $json = json_decode(file_get_contents(__DIR__ . "/test_data/" . $json_file));
-            if (is_null($json)) {
-                throw new Exception("Failed to parse $json_file as json");
-            }
-            // Catch any validation error so that we can re-throw it with
-            // more debug information (i.e. print which test file failed).
-            try {
-                $params = [ 'json' => $json ];
-                $expected = array(
-                    'schema' => array('json' => 'https://api.helioviewer.org/schema/client_state.schema.json')
-                );
-                $optional = [];
-
-                Validation_InputValidator::checkInput($expected, $params, $optional);
-                if ($expected_result === SHOULD_FAIL) {
-                    throw new Exception("Passed on '$json_file' when it should have failed");
-                }
-            } catch (InvalidArgumentException $e) {
-                if ($expected_result === SHOULD_PASS) {
-                    throw new InvalidArgumentException("Failed on '$json_file' when it should have passed: " . $e->getMessage());
-                }
-            }
-            // Mark this test as "not risky"
-            // This test works by throwing an exception on failure.
-            // No exceptions means that it passed.
-            $this->expectNotToPerformAssertions();
+        $json = json_decode(file_get_contents(__DIR__ . "/test_data/" . $json_file));
+        if (is_null($json)) {
+            throw new Exception("Failed to parse $json_file as json");
         }
+        // Catch any validation error so that we can re-throw it with
+        // more debug information (i.e. print which test file failed).
+        try {
+            $params = [ 'json' => $json ];
+            $expected = array(
+                'schema' => array('json' => 'https://api.helioviewer.org/schema/client_state.schema.json')
+            );
+            $optional = [];
+
+            Validation_InputValidator::checkInput($expected, $params, $optional);
+            if ($expected_result === SHOULD_FAIL) {
+                throw new Exception("Passed on '$json_file' when it should have failed");
+            }
+        } catch (InvalidArgumentException $e) {
+            if ($expected_result === SHOULD_PASS) {
+                throw new InvalidArgumentException("Failed on '$json_file' when it should have passed: " . $e->getMessage());
+            }
+        }
+        // Mark this test as "not risky"
+        // This test works by throwing an exception on failure.
+        // No exceptions means that it passed.
+        $this->expectNotToPerformAssertions();
     }
 }
