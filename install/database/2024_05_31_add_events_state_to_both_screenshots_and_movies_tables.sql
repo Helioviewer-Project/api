3748cf930476fc32d1d41a50847571ede4a9c0fe
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-06-21 11:34:09 -0400

    fix sql

--- install/database/2024_05_31_add_events_state_to_both_screenshots_and_movies_tables.sql
+++ install/database/2024_05_31_add_events_state_to_both_screenshots_and_movies_tables.sql
@@ -1,14 +1,14 @@
 -- Add eventState JSON column with default JSON value
-ALTER TABLE screenshots ADD COLUMN eventsState JSON NOT NULL DEFAULT '{}' AFTER eventsLabels;
+ALTER TABLE screenshots ADD COLUMN eventsState JSON NOT NULL DEFAULT ('{}') AFTER eventsLabels;
 
 -- Update existing rows to have the eventsState as {}
-UPDATE screenshots SET eventsState = '{}' WHERE eventsState IS NULL OR eventsState = '';
+UPDATE screenshots SET eventsState = ('{}') WHERE eventsState IS NULL OR eventsState = '';
 
 -- Add eventState JSON column with default JSON value
-ALTER TABLE movies ADD COLUMN eventsState JSON NOT NULL DEFAULT '{}' AFTER eventsLabels;
+ALTER TABLE movies ADD COLUMN eventsState JSON NOT NULL DEFAULT ('{}') AFTER eventsLabels;
 
 -- Update existing rows to have the eventsState as {}
-UPDATE movies SET eventsState = '{}' WHERE eventsState IS NULL OR eventsState = '';
+UPDATE movies SET eventsState = ('{}') WHERE eventsState IS NULL OR eventsState = '';
 
 
 
