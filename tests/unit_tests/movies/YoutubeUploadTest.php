72fc25b87df991e5f6df1b30e43091cc910abea8
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-07-29 14:23:36 -0400

    Fix problem with checkYoutubeAuth (#407)
    * Remove deprecated Google_IO_Curl
    
    * Add sanity check on CheckYoutubeAuth

---
+++ tests/unit_tests/movies/YoutubeUploadTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * @author Daniel Garcia-Briseno <daniel.garciabriseno@nasa.gov>
+ */
+
+use PHPUnit\Framework\TestCase;
+
+final class YoutubeUploadTest extends TestCase
+{
+    /**
+     * Covers https://github.com/Helioviewer-Project/helioviewer.org/issues/592
+     * Youtube Upload failed from check youtube auth due to incorrect use of
+     * Google API.
+     * @runInSeparateProcess
+     */
+    public function testCheckYoutubeAuth() {
+        $params = array("action" => "checkYoutubeAuth");
+        $movies = new Module_Movies($params);
+        ob_start();
+        $movies->execute();
+        $output = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals("false", $output);
+    }
+}
+
