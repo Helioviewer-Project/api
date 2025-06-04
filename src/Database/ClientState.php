6012bfeab09113669e83a22d9a56adc6e130184e
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2025-06-04 15:36:00 +0000

    new functions to read and update client states, and main script to update clientstates with new state vars

--- src/Database/ClientState.php
+++ src/Database/ClientState.php
@@ -58,4 +58,55 @@ class ClientState extends Database_DbConnection
 
     }
 
+    /**
+     * Retrieves all client states with a limit
+     *
+     * @param int $limit The maximum number of client states to retrieve
+     * @return array|null An array of client states or null if none are found
+     */
+    public function all(int $limit = 100): ?array 
+    {
+        $all_sql = "SELECT * FROM client_states LIMIT $limit";
+
+        $query_result = $this->query($all_sql);
+
+        $client_states = $query_result->fetch_all(MYSQLI_ASSOC); 
+
+        $query_result->close();
+
+        return $client_states;
+    }
+
+    
+    /**
+     * This function updates client state without updating its key
+     * @param string, state_key is the id of the client state in database 
+     * @return void
+     */
+    public function update(string $state_key, array $state): bool 
+    {
+        $find_sql = "SELECT * FROM client_states WHERE id = '%s' LIMIT 1";
+
+        $find_state_sql = sprintf($find_sql, $this->link->real_escape_string($state_key));
+
+        $query_result = $this->query($find_state_sql);
+
+        if ($query_result->num_rows == 0) {
+            $query_result->close();
+            return false;
+        }
+
+        $update_sql = "UPDATE client_states SET state = '%s' WHERE id = '%s'";
+
+        $update_state_sql = sprintf($update_sql, $this->link->real_escape_string(json_encode($state)), $this->link->real_escape_string($state_key));
+
+        $this->query($update_state_sql);
+
+        $query_result->close();
+
+        return true;
+    }
+
+
+
 }
