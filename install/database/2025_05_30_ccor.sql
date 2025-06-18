2ac398b34bbf136c3ef4c1c223917082013532a4
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-06-18 14:21:52 -0400

    Rename GOES-R/GOES-19 to GOES

--- install/database/2025_05_30_ccor.sql
+++ install/database/2025_05_30_ccor.sql
@@ -5,7 +5,7 @@ VALUES
 
 INSERT INTO datasource_property (sourceId, label, name, fitsName, description, uiOrder)
 VALUES
-(132, 'Observatory', 'GOES-19' ,'GOES-19', 'GOES Observatory',1),
+(132, 'Observatory', 'GOES' ,'GOES-19', 'GOES-19 Observatory',1),
 (132, 'Instrument', 'CCOR-1', 'CCOR1', 'CCOR-1',2),
 (132, 'Detector', 'CCOR-1', '1', 'CCOR-1',3),
 (132, 'Measurement', 'white-light', 'white-light', 'White Light',4),
@@ -14,3 +14,7 @@ VALUES
 (133, 'Instrument', 'CCOR-2', 'CCOR2', 'CCOR-2',2),
 (133, 'Detector', 'CCOR-2', '1', 'CCOR-2',2),
 (133, 'Measurement', 'white-light', 'white-light', 'White Light',3);
+
+-- Rename GOES-R to GOES for UI purposes.
+-- For data selection, We want both SUVI and CCOR-1 to be under "GOES"
+UPDATE datasource_property SET name = 'GOES' WHERE label = 'Observatory' and sourceId between 2000 and 2005;
