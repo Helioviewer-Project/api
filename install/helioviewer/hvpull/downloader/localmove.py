1cd3ee6ff078746873d6f57db2703e49af586e39
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2026-02-04 12:56:52 -0500

    Prevent localmove.py from excess logging

--- install/helioviewer/hvpull/downloader/localmove.py
+++ install/helioviewer/hvpull/downloader/localmove.py
@@ -6,6 +6,9 @@ import time
 import shutil
 from .downloader_interface import Downloader
 
+# Maximum number of retry attempts per file before giving up
+MAX_RETRY_ATTEMPTS = 3
+
 class LocalFileMove(Downloader):
     def __init__(self, incoming, queue):
         """Creates a new LocalFileMover"""
@@ -16,9 +19,17 @@ class LocalFileMove(Downloader):
         # self.queue
 
     def process(self, item):
-        """Downloads the file at the specified URI"""
-        #ValueError: too many values to unpack
-        server, percent, uri = item
+        """Downloads the file at the specified URI
+
+        Item format: [server, percent, uri] or [server, percent, uri, retry_count]
+        The retry_count is optional and defaults to 0 for backward compatibility.
+        """
+        # Support both old format (3 elements) and new format (4 elements with retry count)
+        if len(item) == 3:
+            server, percent, uri = item
+            retry_count = 0
+        else:
+            server, percent, uri, retry_count = item
 
         # @TODO: compute path to download file to...
 
@@ -38,8 +49,14 @@ class LocalFileMove(Downloader):
             t2 = time.time()
             logging.info("(%s) locally moved %s to %s", server, uri, filepath)
         except IOError:
-            # If download fails, add back into queue and try again later
-            logging.warning("Failed to move %s. Adding to end of queue to retry later.", uri)
-            self.queue.put([server, percent, uri])
+            # If move fails, check retry count before re-queuing
+            if retry_count < MAX_RETRY_ATTEMPTS:
+                retry_count += 1
+                logging.warning("Failed to move %s. Adding to end of queue to retry later (attempt %d/%d).",
+                               uri, retry_count, MAX_RETRY_ATTEMPTS)
+                self.queue.put([server, percent, uri, retry_count])
+            else:
+                logging.error("Failed to move %s after %d attempts. Giving up on this file.",
+                             uri, MAX_RETRY_ATTEMPTS)
         except:
             logging.warning("Failed to move %s.", uri)
