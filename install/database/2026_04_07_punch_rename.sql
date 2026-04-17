264ddfebf427f51be4c85e782d6fe5ad21e96f0d
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2026-04-17 08:56:27 -0400

    Update sidebar label to Measurement: Total/Polarized brightness

--- install/database/2026_04_07_punch_rename.sql
+++ install/database/2026_04_07_punch_rename.sql
@@ -3,7 +3,7 @@ UPDATE datasources SET description = 'PUNCH WFI+NFI CAM Mosaic' WHERE id = 131;
 UPDATE datasource_property SET name = 'WFI+NFI', fitsName = 'WFI+NFI', description = 'Clear low-noise science mosaic, bkg-sub & resolved into B & uncertainty layer' WHERE sourceId = 131 AND label = 'Instrument';
 
 INSERT INTO datasource_property (sourceId, label, name, fitsName, description, uiOrder)
-VALUES (131, 'Product', 'CAM', 'PUNCH Level-3 Intermediate F-corona Subtracted Unpolarized Mosaic', '', 3);
+VALUES (131, 'Measurement', 'Total Brightness', 'PUNCH Level-3 Intermediate F-corona Subtracted Unpolarized Mosaic', '', 3);
 
 INSERT INTO datasources (id, name, description, units, layeringOrder, enabled, sourceIdGroup, displayOrder)
 VALUES (134, 'PUNCH', 'PUNCH WFI+NFI PAM Mosaic', NULL, 1, 0, '', 0);
@@ -12,4 +12,4 @@ INSERT INTO datasource_property (sourceId, label, name, fitsName, description, u
 VALUES
 (134, 'Observatory', 'PUNCH', 'PUNCH', 'PUNCH', 1),
 (134, 'Instrument', 'WFI+NFI', 'WFI+NFI', 'Polarized low-noise science mosaic, bkg-sub & resolved into B, pB, & uncertainty layer', 2),
-(134, 'Product', 'PAM', 'PUNCH Level-3 Intermediate F-corona Subtracted Polarized Mosaic', '', 3);
+(134, 'Measurement', 'Polarized Brightness', 'PUNCH Level-3 Intermediate F-corona Subtracted Polarized Mosaic', '', 3);
