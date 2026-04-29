1651c300862ffc5ae79f14f233618d8a7fd3cd49
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2026-04-29 14:48:07 -0400

    Update punch unit test

--- install/__test__/test_punch.py
+++ install/__test__/test_punch.py
@@ -7,16 +7,16 @@ class TestPunchDataServer(unittest.TestCase):
     punch = PUNCHDataServer()
     dirs = punch.compute_directories(datetime.datetime(2026, 1, 1), datetime.datetime(2026, 1, 5))
     assert dirs == [
-      'https://umbra.nascom.nasa.gov/punch/3/CAM/2026/01/05',
-      'https://umbra.nascom.nasa.gov/punch/3/PAM/2026/01/05',
-      'https://umbra.nascom.nasa.gov/punch/3/CAM/2026/01/04',
-      'https://umbra.nascom.nasa.gov/punch/3/PAM/2026/01/04',
-      'https://umbra.nascom.nasa.gov/punch/3/CAM/2026/01/03',
-      'https://umbra.nascom.nasa.gov/punch/3/PAM/2026/01/03',
-      'https://umbra.nascom.nasa.gov/punch/3/CAM/2026/01/02',
-      'https://umbra.nascom.nasa.gov/punch/3/PAM/2026/01/02',
-      'https://umbra.nascom.nasa.gov/punch/3/CAM/2026/01/01',
-      'https://umbra.nascom.nasa.gov/punch/3/PAM/2026/01/01',
+      'https://umbra.nascom.nasa.gov/punch/L/3/CAM/2026/01/05',
+      'https://umbra.nascom.nasa.gov/punch/L/3/PAM/2026/01/05',
+      'https://umbra.nascom.nasa.gov/punch/L/3/CAM/2026/01/04',
+      'https://umbra.nascom.nasa.gov/punch/L/3/PAM/2026/01/04',
+      'https://umbra.nascom.nasa.gov/punch/L/3/CAM/2026/01/03',
+      'https://umbra.nascom.nasa.gov/punch/L/3/PAM/2026/01/03',
+      'https://umbra.nascom.nasa.gov/punch/L/3/CAM/2026/01/02',
+      'https://umbra.nascom.nasa.gov/punch/L/3/PAM/2026/01/02',
+      'https://umbra.nascom.nasa.gov/punch/L/3/CAM/2026/01/01',
+      'https://umbra.nascom.nasa.gov/punch/L/3/PAM/2026/01/01',
     ]
 
   def test_get_datetime_from_file(self):
