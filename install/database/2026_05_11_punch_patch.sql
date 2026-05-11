170d8884ac2d84734cf2c3fb0c4d41cd3cc36155
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2026-05-11 10:58:09 -0400

    Add SQL to support some PUNCH files (#485)
    Co-authored-by: Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>

---
+++ install/database/2026_05_11_punch_patch.sql
@@ -0,0 +1,5 @@
+INSERT INTO datasource_property(sourceId, label, name, fitsName, description, uiOrder)
+VALUES (131, 'Measurement', 'Total Brightness', 'PUNCH Level-3 Unpolarized Mosaic', '', 3);
+
+INSERT INTO datasource_property(sourceId, label, name, fitsName, description, uiOrder)
+VALUES (134, 'Measurement', 'Polarized Brightness', 'PUNCH Level-3 Polarized Mosaic', '', 3);
